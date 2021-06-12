5b70adf2e9d8caa1e3603f3bf1200d8c9cbd0d13
nulysses <adr.zivil@gmail.com>
2021-06-13 01:13:59 +0200

    1

--- ar18_update/ar18_update.sh
+++ ar18_update/ar18_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ar18#
+# ar18
 
 # Script template version 2021-06-13
 # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
@@ -47,6 +47,7 @@ git clone https://github.com/ar18-linux/xfce_desktop_deployment.git
 
 xfce_desktop_deployment/xfce_desktop_deployment/exec.sh
 
+# TODO: Use a global variable for it so the parent and other children know that the cache was updated. Alternatively, look for a way to checkif cache was updated recently in pacman itsself
 echo "${ar18_pacman_cache_updated}"
 
 ##################################SCRIPT_END###################################
