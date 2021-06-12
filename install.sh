4b0c5c3ce6d5f9a31531476f0903756764441b94
nulysses <adr.zivil@gmail.com>
2021-06-12 18:55:31 +0200

    1

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ if [ -z "${ar18_exit_map+x}" ]; then
 fi
 ar18_exit_map["${script_path}"]=0
 # Get old shell option values to restore later
+shopt -s inherit_errexit
 IFS=$'\n' shell_options=($(shopt -op))
 # Set shell options for this script
 set -o pipefail
@@ -34,8 +35,7 @@ set -x
 . "${script_dir}/vars"
 if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
 obtain_sudo_password
-shopt -op
-ls dsfhdsi
+
 ar18_install "${install_dir}" "${module_name}" "${script_dir}"
 
 # End of script
