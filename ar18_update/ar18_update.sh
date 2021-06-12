3c76f84b3a7787cfb2bd3ad7b8f38e6b2b5f7c3f
nulysses <adr.zivil@gmail.com>
2021-06-12 17:55:15 +0200

    1

--- ar18_update/ar18_update.sh
+++ ar18_update/ar18_update.sh
@@ -7,7 +7,8 @@ LD_PRELOAD=
 # Determine the full path of the directory this script is in
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 script_path="${script_dir}/$(basename "${0}")"
-echo "script_path: ${script_path}"
+#Set PS4 for easier debugging
+export PS4='$(basename "${0}"):${LINENO}: '
 # Determine if this script was sourced or is the parent script
 if [ -z "${ar18_sourced_map+x}" ]; then
   declare -A -g ar18_sourced_map
@@ -17,6 +18,7 @@ if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
 else
   ar18_sourced_map["${script_path}"]=0
 fi
+echo "sourced: ${ar18_sourced_map["${script_path}"]}" 
 # Initialise exit code
 if [ -z "${ar18_exit_map+x}" ]; then
   declare -A -g ar18_exit_map
@@ -41,7 +43,9 @@ cd /tmp/ar18_update
 
 git clone https://github.com/ar18-linux/xfce_desktop_deployment.git
 
+echo "sourced: ${ar18_sourced_map["${script_path}"]}" 
 . xfce_desktop_deployment/xfce_desktop_deployment/exec.sh
+echo "sourced: ${ar18_sourced_map["${script_path}"]}" 
 
 # End of script
 # Restore old shell values
@@ -52,6 +56,7 @@ done
 # Restore LD_PRELOAD
 LD_PRELOAD="${LD_PRELOAD_old}"
 # Return or exit depending on whether the script was sourced or not
+echo "sourced: ${ar18_sourced_map["${script_path}"]}" 
 if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
   return "${ar18_exit_map["${script_path}"]}"
 else
