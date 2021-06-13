34e8dd7bc6b9cc45067c2f28fa9ffd4164e3941b
nulysses <adr.zivil@gmail.com>
2021-06-13 12:51:33 +0200

    1

--- ar18_update/ar18_update.sh
+++ ar18_update/ar18_update.sh
@@ -34,9 +34,10 @@ set -eu
 
 set -x
 
-. "${script_dir}/vars"
 if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
 obtain_sudo_password
+
+import_vars
 
 echo "${ar18_sudo_password}" | sudo -Sk rm -rf /tmp/ar18_update
 mkdir -p /tmp/ar18_update
