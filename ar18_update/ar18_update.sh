923a0a61a4e886831e905dfb18ac7e4d36ae5e32
nulysses <adr.zivil@gmail.com>
2021-07-06 07:54:28 +0200

    1

--- ar18_update/ar18_update.sh
+++ ar18_update/ar18_update.sh
@@ -1,67 +1,92 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # ar18
 
-# Script template version 2021-06-13
-# Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
-LD_PRELOAD_old="${LD_PRELOAD}"
-LD_PRELOAD=
-# Determine the full path of the directory this script is in
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-script_path="${script_dir}/$(basename "${0}")"
-#Set PS4 for easier debugging
-export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
-# Determine if this script was sourced or is the parent script
-if [ ! -v ar18_sourced_map ]; then
-  declare -A -g ar18_sourced_map
-fi
-if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-  ar18_sourced_map["${script_path}"]=1
-else
-  ar18_sourced_map["${script_path}"]=0
-fi
-# Initialise exit code
-if [ -z "${ar18_exit_map+x}" ]; then
-  declare -A -g ar18_exit_map
-fi
-ar18_exit_map["${script_path}"]=0
-# Get old shell option values to restore later
-shopt -s inherit_errexit
-IFS=$'\n' shell_options=($(shopt -op))
-# Set shell options for this script
-set -o pipefail
-set -eu
+# Prepare script environment
+{
+  # Script template version 2021-07-06_00:10:24
+  # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
+  LD_PRELOAD_old="${LD_PRELOAD}"
+  LD_PRELOAD=
+  # Determine the full path of the directory this script is in
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  script_path="${script_dir}/$(basename "${0}")"
+  #Set PS4 for easier debugging
+  export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
+  # Determine if this script was sourced or is the parent script
+  if [ ! -v ar18_sourced_map ]; then
+    declare -A -g ar18_sourced_map
+  fi
+  if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    ar18_sourced_map["${script_path}"]=1
+  else
+    ar18_sourced_map["${script_path}"]=0
+  fi
+  # Initialise exit code
+  if [ ! -v ar18_exit_map ]; then
+    declare -A -g ar18_exit_map
+  fi
+  ar18_exit_map["${script_path}"]=0
+  # Save PWD
+  if [ ! -v ar18_pwd_map ]; then
+    declare -A -g ar18_pwd_map
+  fi
+  ar18_pwd_map["${script_path}"]="${PWD}"
+  # Get old shell option values to restore later
+  shopt -s inherit_errexit
+  IFS=$'\n' shell_options=($(shopt -op))
+  # Set shell options for this script
+  set -o pipefail
+  set -eu
+  if [ ! -v ar18_parent_process ]; then
+    export ar18_parent_process="$$"
+  fi
+  # Get import module
+  if [ ! -v ar18.script.import ]; then
+    mkdir -p "/tmp/${ar18_parent_process}"
+    cd "/tmp/${ar18_parent_process}"
+    curl -O https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/script/import.sh > /dev/null 2>&1 && . "/tmp/${ar18_parent_process}/import.sh"
+    cd "${ar18_pwd_map["${script_path}"]}"
+  fi
+}
 #################################SCRIPT_START##################################
 
-set -x
+ar18.script.import ar18.script.obtain_sudo_password
+ar18.script.import ar18.script.import_vars
+ar18.script.import ar18.script.execute_with_sudo
 
-if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
-obtain_sudo_password
+ar18.script.obtain_sudo_password
 
-import_vars
+ar18.script.execute_with_sudo rm -rf "/tmp/ar18_update"
+mkdir -p "/tmp/ar18_update"
 
-echo "${ar18_sudo_password}" | sudo -Sk rm -rf /tmp/ar18_update
-mkdir -p /tmp/ar18_update
-
-cd /tmp/ar18_update
+cd "/tmp/ar18_update"
 
 git clone https://github.com/ar18-linux/deploy.git
 
-deploy/deploy/exec.sh
+ar18.script.execute_with_sudo chmod +x "/tmp/ar18_update/deploy/deploy/exec.sh"
+"/tmp/ar18_update/deploy/deploy/exec.sh"
 
 # TODO: Use a global variable for it so the parent and other children know that the cache was updated. Alternatively, look for a way to checkif cache was updated recently in pacman itsself
 #echo "${ar18_pacman_cache_updated}"
 
 ##################################SCRIPT_END###################################
-# Restore old shell values
-set +x
-for option in "${shell_options[@]}"; do
-  eval "${option}"
-done
-# Restore LD_PRELOAD
-LD_PRELOAD="${LD_PRELOAD_old}"
+# Restore environment
+{
+  # Restore old shell values
+  set +x
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
+  # Restore LD_PRELOAD
+  LD_PRELOAD="${LD_PRELOAD_old}"
+  # Restore PWD
+  cd "${ar18_pwd_map["${script_path}"]}"
+}
 # Return or exit depending on whether the script was sourced or not
-if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
-  return "${ar18_exit_map["${script_path}"]}"
-else
-  exit "${ar18_exit_map["${script_path}"]}"
-fi
+{
+  if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
+    return "${ar18_exit_map["${script_path}"]}"
+  else
+    exit "${ar18_exit_map["${script_path}"]}"
+  fi
+}
